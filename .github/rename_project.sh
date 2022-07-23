0b8a539bbd8d5aa09e447fb797302ad8a0b8ba54
Eyad206 <Eyad206@users.noreply.github.com>
2022-07-23 09:52:32 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="Eyad206"
+original_name="eyad"
+original_urlname="Eyad"
+original_description="Awesome eyad created by Eyad206"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv eyad $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
--- tests/test_base.py
+++ tests/test_base.py
@@ -1,5 +1,5 @@
-from project_name.base import NAME
+from eyad.base import NAME
 
 
 def test_base():
-    assert NAME == "project_name"
+    assert NAME == "eyad"
